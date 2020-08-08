608a05fa5c4315fc0757b4b7f64c23b517538311
Andy Garcia <andy.w.garcia@gmail.com>
2020-08-08 13:51:54 -0700

    Persisting state to local storage

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,18 @@
 import { createStore } from "redux";
 import rootReducer from "./reducers";
+import throttle from "lodash/throttle";
+import { saveState, loadState } from "./localStorage";
 
 const store = createStore(
   rootReducer,
+  loadState(),
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
+
+store.subscribe(
+  throttle(() => {
+    saveState(store.getState());
+  }, 1000)
+);
+
 export default store;
