b53d062fb8ad6a2cb59dae6ed5ff6277a06ea049
Andy Garcia <andy.w.garcia@gmail.com>
2020-08-16 17:34:24 -0700

    Removing some unused code

--- src/redux/selectors/loans.js
+++ src/redux/selectors/loans.js
@@ -2,92 +2,6 @@ import { createSelector } from "reselect";
 import { getDebtPayoffSortFunction } from "./userPreferences";
 import { getSpendingHistory } from "./spending";
 import moment from "moment";
-import { create } from "lodash";
-
-const initalPayoffDetails = {
-  payments: [],
-  totalInterest: 0,
-};
-
-const getPayoffDetails = (currentLoan, desiredSpending) => {
-  // Assumes monthly capitalization
-  if (
-    parseInt(currentLoan.currentBalance || 0) === 0 ||
-    parseInt(currentLoan.interestRate || 0) === 0 ||
-    parseInt(currentLoan.monthlyPayment || 0) === 0
-  ) {
-    return { ...initalPayoffDetails };
-  }
-  let balance = parseFloat(currentLoan.currentBalance);
-  const monthlyRate = currentLoan.interestRate / 12.0 / 100.0;
-  let totalInterest = 0.0;
-  let payments = [];
-  let usedDesiredSpending = false;
-  while (balance.toFixed(2) > 0) {
-    const growthAmount = balance * monthlyRate;
-    const currentMonthPayment =
-      balance - currentLoan.monthlyPayment < 0
-        ? balance + growthAmount
-        : currentLoan.monthlyPayment;
-    let actualMonthlyPaymentSpent = currentMonthPayment;
-    if (growthAmount > currentMonthPayment) {
-      return { ...initalPayoffDetails };
-    }
-
-    totalInterest += growthAmount;
-
-    // Capitalize
-    balance = balance + growthAmount;
-
-    // Pay monthly payment
-    balance = balance - currentMonthPayment;
-
-    // Only use the extra payment once
-    if (!usedDesiredSpending) {
-      balance = balance - (desiredSpending || 0);
-      actualMonthlyPaymentSpent = actualMonthlyPaymentSpent + desiredSpending;
-      usedDesiredSpending = true;
-    }
-    payments.push(actualMonthlyPaymentSpent);
-
-    // Don't let it go longer than 30 years for performance sake
-    if (payments.length > 12 * 30) {
-      return 12 * 30;
-    }
-  }
-  return { payments, totalInterest };
-};
-
-const calculatePayoffDetails = (
-  allLoans,
-  debtSortFunction,
-  desiredSpending = 0
-) => {
-  return [...allLoans].sort(debtSortFunction).reduce(
-    (acc, curr, index) => {
-      const payoffDetails = getPayoffDetails(
-        {
-          currentBalance: curr.balance,
-          interestRate: curr.interestRate,
-          monthlyPayment: curr.monthlyMinimumPayment,
-        },
-        index === 0 ? desiredSpending : 0
-      );
-      return {
-        ...acc,
-        payments: [
-          ...acc.payments,
-          {
-            loanId: curr.id,
-            payments: payoffDetails.payments,
-          },
-        ],
-        totalInterest: acc.totalInterest + payoffDetails.totalInterest,
-      };
-    },
-    { payments: [], totalInterest: 0 }
-  );
-};
 
 /*
 input
@@ -241,20 +155,6 @@ const generatePaymentPlan = (
   }
   return payments;
 };
-
-export const payoffSavingsDetails = createSelector(
-  [
-    (state) => state.loans.allLoans,
-    getDebtPayoffSortFunction,
-    (state) => state.availableAmounts.desiredSpending,
-  ],
-  calculatePayoffDetails
-);
-
-export const originalPayoffDetails = createSelector(
-  [(state) => state.loans.allLoans, getDebtPayoffSortFunction],
-  calculatePayoffDetails
-);
 
 export const getLoan = createSelector(
   [
